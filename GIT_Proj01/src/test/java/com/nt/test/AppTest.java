11f2a553c642dc0fde3bd5ecb6cc1fd12edfae65
HP EliteBook <HP EliteBook@192.168.1.33>
2024-07-12 21:18:49 +0530

    initial project commit and push by the TL

---
+++ GIT_Proj01/src/test/java/com/nt/test/AppTest.java
@@ -0,0 +1,65 @@
+package com.nt.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.nt.service.Arithmetic;
+public class AppTest 
+{
+	private static Arithmetic ar;
+	@Test
+	public void testWithPositivesValues() {
+		System.out.println("AppTest.testWithPositivesValues()");
+		float expected=300.0f;
+		float actual=ar.sum(100.0f, 200.0f);
+		assertEquals(expected, actual,"test1");
+		}
+	@Test
+	public void testWithNegativesValues() {
+		System.out.println("AppTest.testWithNegativesValues()");
+		float expected=-400.0f;
+		float actual=ar.sum(-200.0f, -200.0f);
+		assertEquals(expected, actual,"test2");
+		}
+	@Test
+	public void testWithMixedValues() {
+		
+		System.out.println("AppTest.testWithMixedValues()");
+		float expected=100.0f;
+		float actual=ar.sum(-100.0f, 200.0f);
+		assertEquals(expected, actual,"test3");
+		}
+	@Test
+	public void testWithZeroes() {
+		System.out.println("AppTest.testWithZeroes()");
+		float expected=0.0f;
+		float actual=ar.sum(0.0f, 0.0f);
+		assertEquals(expected, actual,"test4");
+		}
+	@BeforeAll
+	public static void setUp() {
+		System.out.println("AppTest.setUpOnce()::BeforeAll");
+		ar=new Arithmetic();
+	}
+	@AfterAll
+	public static void tearDown() {
+		System.out.println("AppTest.tearDown()");
+		ar=null;
+	}
+	@BeforeEach
+	public void setAtBefore() {
+		System.out.println("AppTest.setAtBefore()");
+		ar=new Arithmetic();
+	}
+	@AfterEach
+	public void setAtAfter() {
+		System.out.println("AppTest.setAtAfter()");
+		ar=new Arithmetic();
+	}
+	
+}
